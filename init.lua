9b6bb33909827192b4caf674d1581e5d87f955f7
joavor <36812452+joavor@users.noreply.github.com>
2024-10-08 11:22:08 +0200

    Changed to tpope's undotree

--- init.lua
+++ init.lua
@@ -438,9 +438,19 @@ require('lazy').setup({
   },
 
   {
-    'jiaoshijie/undotree',
-    dependencies = 'nvim-lua/plenary.nvim',
-    config = true,
+    'mbbill/undotree',
+    opts = {},
+    config = function()
+      vim.cmd 'let g:undotree_WindowLayout = 4'
+      vim.cmd 'let g:undotree_DiffpanelHeight = 20'
+      vim.cmd 'let g:undotree_SplitWidth = 40'
+      vim.cmd 'let g:undotree_HelpLine = 0'
+      vim.cmd 'let g:undotree_SetFocusWhenToggle = 1'
+      vim.cmd 'let g:undotree_TreeVertShape = "|"'
+      vim.cmd 'let g:undotree_TreeNodeShape = "×"'
+      vim.cmd 'let g:undotree_TreeSplitShape = "/"'
+      vim.cmd 'let g:undotree_TreeReturnShape = ""'
+    end,
   },
 
   -- LSP Plugins
